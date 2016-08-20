141cafba225ff175fd07afe4c7afaad08f9b1b0a
qiuhai <55704123ggdd@163.com>
2016-08-20 11:14:39 +0800

    移动函数  Move Method

--- refactor/src/Rental.java
+++ refactor/src/Rental.java
@@ -28,4 +28,26 @@ public class Rental {
     public void setDaysRented(int daysRented) {
         this.daysRented = daysRented;
     }
+
+    public double getCharge() {
+        double result = 0;
+        switch (getMovie().getPriceCode()){
+            case Movie.REGULAR:
+                result += 2;
+                if(getDaysRented()>2){
+                    result += (getDaysRented()-2)*1.5;
+                }
+                break;
+            case Movie.NEW_REALEASE:
+                result += getDaysRented()*3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if(getDaysRented() > 3){
+                    result += (getDaysRented()-3)*1.5;
+                    break;
+                }
+        }
+        return result;
+    }
 }
