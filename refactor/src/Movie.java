c50cae6d7ee380a6bbe4d9cf0c90fd1c012489d6
qiuhai <55704123ggdd@163.com>
2016-08-20 13:28:49 +0800

    运用多态取代与价格相关的条件逻辑

--- refactor/src/Movie.java
+++ refactor/src/Movie.java
@@ -17,6 +17,38 @@ public class Movie {
         this.priceCode = priceCode;
     }
 
+
+    public double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()){
+            case Movie.REGULAR:
+                result += 2;
+                if(daysRented>2){
+                    result += (daysRented-2)*1.5;
+                }
+                break;
+            case Movie.NEW_REALEASE:
+                result += daysRented*3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if(daysRented > 3){
+                    result += (daysRented-3)*1.5;
+                    break;
+                }
+        }
+        return result;
+    }
+
+    public int getFrequentRenterPoints(int daysRented){
+
+        if(getPriceCode()==Movie.NEW_REALEASE&&daysRented>1){
+            return 2;
+        }else {
+            return 1;
+        }
+    }
+
     public String getTitle() {
         return title;
     }
